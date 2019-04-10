f4b527ea35e0cd005b6d8a1f0b5ff63f0f9d183a
dzoli <bje.no9@gmail.com>
2019-04-10 11:39:49 +0200

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api/'
+  //   apiUrl: 'http://localhost:8000/api/'
+  apiUrl: 'https://blooming-retreat-35769.herokuapp.com/api/'
 };
